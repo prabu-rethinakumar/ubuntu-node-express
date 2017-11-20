ecda390ffe2222b971f50bd458e7c88b0abf56b2
ubuntu <ubuntu@ld03846.homedepot.com>
2017-11-20 15:11:37 -0500

    First commit

--- exec.sh
+++ exec.sh
@@ -3,7 +3,12 @@
 sudo apt-get update
 sudo apt-get install git 
 
+mkdir code 
 
-
-
- 
+git init
+git remote add master 'https://github.com/prabu-rethinakumar/ubuntu-node-express.git'
+git remote -v
+git add --all
+git commit 'First commmit'
+git push --set-upstream master
+git push master master 
